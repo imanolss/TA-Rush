c717074eb55ef8bef8b15019d11d35c83549267f
Imanol Sanchez <you@example.com>
2019-08-27 17:46:07 -0500

    assets del juego

---
+++ main.js
@@ -0,0 +1,20 @@
+// Collect The Square game
+
+// Get a reference to the canvas DOM element
+const canvas = document.getElementById('canvas')
+// Get the canvas drawing context
+const context = canvas.getContext('2d')
+//Canvas7
+/*
+function portada() {
+  imgPortada = new Image()
+  imgPortada.src = 'images/portada_game.png'
+  imgPortada.onload = function() {
+    context.drawImage(imgPortada, 0, 0)
+  }
+}
+*/
+window.onload = function() {
+  let portada = document.getElementById('canvas')
+  let ctx = portada.getContext('2d')
+}
